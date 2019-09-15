374dbe677e9852e48bcca9552535a72ce942c54e
Gabriel Brunichaki <gabriel.brunichaki@acad.pucrs.br>
2019-09-15 01:00:26 -0300

    fix structure and routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,26 +2,13 @@ import React from 'react';
 import { Switch, Redirect } from 'react-router-dom';
 
 import { RouteWithLayout } from './components';
-import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
+import { Main } from './layouts';
+import { Dashboard } from './views';
 
-import {
-  Dashboard as DashboardView,
-  ProductList as ProductListView,
-  UserList as UserListView,
-  Typography as TypographyView,
-  Icons as IconsView,
-  Account as AccountView,
-  Settings as SettingsView,
-  SignUp as SignUpView,
-  SignIn as SignInView,
-  NotFound as NotFoundView,
-  ChartsPAHO as ChartsPAHOView
-} from './views';
-
-import Disasters from './pages/Disasters';
-import Plans from './pages/Plans';
-import Map from './pages/Map';
-import Settings from './pages/Settings';
+import Disasters from './disasters/Disasters';
+import Plans from './plans/Plans';
+import Map from './map/Map';
+import Settings from './settings/Settings';
 
 const Routes = () => {
   return (
@@ -32,90 +19,30 @@ const Routes = () => {
         to="/dashboard"
       />
       <RouteWithLayout
+        component={Dashboard}
+        exact
+        layout={Main}
+        path="/dashboard"
+      />
+      <RouteWithLayout
         exact path="/disasters"
-        layout={MainLayout}
+        layout={Main}
         component={Disasters}
       />
       <RouteWithLayout
         exact path="/plans"
-        layout={MainLayout}
+        layout={Main}
         component={Plans}
       />
       <RouteWithLayout
         exact path="/map"
-        layout={MainLayout}
+        layout={Main}
         component={Map}
       />
       <RouteWithLayout
         exact path="/settings"
-        layout={MainLayout}
+        layout={Main}
         component={Settings}
-      />
-      <RouteWithLayout
-        component={DashboardView}
-        exact
-        layout={MainLayout}
-        path="/dashboard"
-      />
-      <RouteWithLayout
-        component={UserListView}
-        exact
-        layout={MainLayout}
-        path="/users"
-      />
-      <RouteWithLayout
-        component={ProductListView}
-        exact
-        layout={MainLayout}
-        path="/products"
-      />
-      <RouteWithLayout
-        component={TypographyView}
-        exact
-        layout={MainLayout}
-        path="/typography"
-      />
-      <RouteWithLayout
-        component={IconsView}
-        exact
-        layout={MainLayout}
-        path="/icons"
-      />
-      <RouteWithLayout
-        component={AccountView}
-        exact
-        layout={MainLayout}
-        path="/account"
-      />
-      <RouteWithLayout
-        component={SettingsView}
-        exact
-        layout={MainLayout}
-        path="/settings"
-      />
-      <RouteWithLayout
-        component={SignUpView}
-        exact
-        layout={MinimalLayout}
-        path="/sign-up"
-      />
-      <RouteWithLayout
-        component={SignInView}
-        exact
-        layout={MinimalLayout}
-        path="/sign-in"
-      />
-      <RouteWithLayout
-        component={NotFoundView}
-        exact
-        layout={MinimalLayout}
-        path="/not-found"
-      />
-      <RouteWithLayout
-        component={ChartsPAHOView}
-        exact
-        layout={MainLayout}
-        path="/chartsPAHO"
       />
       <Redirect to="/not-found" />
     </Switch>
